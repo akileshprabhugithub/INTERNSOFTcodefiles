e2d77deaf3024827d8bef1dfbdffee6efda22263
Akilesh S Prabhu <31615067+akileshprabhugithub@users.noreply.github.com>
2020-08-16 22:38:47 +0530

    Add files via upload

---
+++ SESSION11intro_spyder.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+batman = pd.read_csv('AAPL Historical Data.csv',usecols = [0,1,2,3,4])
+
+POHL_avg = batman[['Price','Open','High','Low']].mean(axis=1)
+
+obs = np.arange(1,len(batman)+1,1) 
+
+plt.plot(obs,POHL_avg,'r',label="my first plot")
